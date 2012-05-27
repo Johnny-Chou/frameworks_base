069687eb16a243148759b1f5571b3c4ac091aa87
stimpz0r <stimpz0r@kriticalcrew.org>
2012-05-27 23:10:02 +1000

    added 4th button in TabletStatusBar.jar for menu button.

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -145,15 +145,15 @@ public class TabletStatusBar extends StatusBar implements
     final static String ACTION_KILL = "**kill**";
     final static String ACTION_NULL = "**null**";
 
-    int mNumberOfButtons = 3;
+    int mNumberOfButtons = 4;
 
     public String[] mClickActions = new String[5];
     public String[] mLongpressActions = new String[5];
     public String[] mPortraitIcons = new String[5];
 
-    public final static int StockButtonsQty = 3;
+    public final static int StockButtonsQty = 4;
     public final static String[] StockClickActions = {
-            "**back**", "**home**", "**recents**", "**null**", "**null**"
+            "**back**", "**home**", "**recents**", "**menu**", "**null**"
     };
 
     public final static String[] StockLongpress = {
